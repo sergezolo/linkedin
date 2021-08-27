07677278e5d564af524f3c812b4e930237ffbd1a
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2021-08-27 00:23:11 -0500

    Add Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import './Header.css';
+import HeaderOption from './HeaderOption';
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import GroupIcon from '@material-ui/icons/Group';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import SmsIcon from '@material-ui/icons/Sms';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
 
 function Header() {
     return (
         <div className="header">
-            <h1>Header</h1>
-
             <div className="header-left">
-                <img src="" alt="" />
+                <img 
+                    src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" alt="linkedin-icon" 
+                />
 
                 <div className="header-search">
-                    
+                    <SearchIcon />
+                    <input type="text" />
                 </div>
             </div>
 
             <div className="header-right">
-
+                <HeaderOption Icon={HomeIcon} title="Home"/>
+                <HeaderOption Icon={GroupIcon} title="My Network"/>
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
+                <HeaderOption Icon={SmsIcon} title="Messaging"/>
+                <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
+                <HeaderOption avatar="https://media-exp1.licdn.com/dms/image/C4E03AQFzG-gEFc2I2Q/profile-displayphoto-shrink_100_100/0/1613158890439?e=1635379200&v=beta&t=8HQ9d0vILon-tvZhVRuFvohsucNWCck6MH_nVjBbJbs" title="me" />
             </div>
         </div>
     )
---
+++ src/components/HeaderOption.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './HeaderOption.css';
+import { Avatar } from "@material-ui/core";
+
+function HeaderOption({ avatar, Icon, title }) {
+    return (
+        <div className='header-option'>
+            {Icon && <Icon className="header-option-icon"/>}
+            {avatar && <Avatar className="header-option-icon" src={avatar}/>}
+            <h3 className="header-option-title">{title}</h3>
+        </div>
+    )
+}
+
+export default HeaderOption;
