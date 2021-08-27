33497f95b8d1f2b4cdc04a54a2cff01c6078af87
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2021-08-26 23:09:40 -0500

    Initialize project with basic set-up

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Header</h1>
+
+            <div className="header-left">
+                <img src="" alt="" />
+
+                <div className="header-search">
+                    
+                </div>
+            </div>
+
+            <div className="header-right">
+
+            </div>
+        </div>
+    )
+}
+
+export default Header;
